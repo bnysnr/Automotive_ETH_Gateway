7bcaa2f32356546218fb84a4a2d3c9d7e77955d0
bnysnr <bunyaminsener03@gmail.com>
2025-11-25 15:19:45 +0100

    Aumovio Schriftart + Farbdarstellung der Statuswerte implementiert

--- script/SignalStateGUI.py
+++ script/SignalStateGUI.py
@@ -1,21 +1,49 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QLabel, QTableWidget, QTableWidgetItem, QHeaderView, QAbstractItemView
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QFontDatabase, QFont, QColor, QBrush
+
+
+def apply_font_to_table(table, font):
+        table.setFont(font)
+        table.horizontalHeader().setFont(font)
+        table.verticalHeader().setFont(font)
+        for r in range(table.rowCount()):
+            for c in range(table.columnCount()):
+                item = table.item(r, c)
+                if item:
+                    item.setFont(font)
 
 
 class MyWindow(QWidget):
+
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Signal State Dashboard")
-        self.setFixedSize(1200, 450)
 
+        # --- globale Font laden ---
+        font_id = QFontDatabase.addApplicationFont('/home/admin/Praxissemester/font/TTF/AUMOVIOOffice-Regular.ttf')
+        if font_id == -1:
+            print("Fehler beim Laden der Schriftart!")
+            self.gui_font = QFont()
+        else:
+            font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+            self.gui_font = QFont(font_family, 12)
+            QApplication.instance().setFont(self.gui_font)
+
+        # --- Fenster-Einstellungen ---
+        self.setWindowTitle("Signal State Dashboard")
+        self.setFixedSize(1200, 475)
+
+        # --- GUI erstellen ---
         self.titleBox()
         self.egomotionBox()
         self.sensorconfigmsgstatusbox()
 
+        apply_font_to_table(self.table_ego, self.gui_font)
+        apply_font_to_table(self.table_cfg, self.gui_font)
 
 
+    # ---------------- Widgets ----------------
     def titleBox(self):
         self.box1 = QFrame(self)
         self.box1.setFrameShape(QFrame.Box)
@@ -26,7 +54,6 @@ class MyWindow(QWidget):
 
         self.logo_label = QLabel(self.box1)
         self.logo_label.setGeometry(5, 5, 200, box_height)
-
         pix = QPixmap("/home/admin/Praxissemester/script/Aumovio_Logo_orange_black_transparent.png")
         if not pix.isNull():
             scaled_pix = pix.scaled(self.logo_label.width(), self.logo_label.height(), Qt.KeepAspectRatio)
@@ -36,7 +63,6 @@ class MyWindow(QWidget):
         self.label1.setStyleSheet("font-size: 24px; font-weight: bold;")
         self.label1.setAlignment(Qt.AlignCenter)
         self.label1.setGeometry(0, 0, self.width(), box_height)
-
 
 
     def egomotionBox(self):
@@ -55,8 +81,7 @@ class MyWindow(QWidget):
         self.table_ego.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         header = self.table_ego.horizontalHeader()
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QHeaderView.Stretch)
+        header.setSectionResizeMode(QHeaderView.Fixed)
 
         ego_names = [
             "Yaw Rate", "Steering Wheel Angle", "Lateral Acceleration",
@@ -66,15 +91,17 @@ class MyWindow(QWidget):
         ]
 
         for i, name in enumerate(ego_names):
-            self.table_ego.setItem(i, 0, QTableWidgetItem(name))
+            item = QTableWidgetItem(name)
+            item.setFont(self.gui_font)
+            self.table_ego.setItem(i, 0, item)
 
         box_y = int(self.height() * 0.1) + 10
         box_height = int(self.height() * 0.85)
         usable_width = self.width() - 30
-        box2_width = int(usable_width * 0.6)
+        box2_width = int(usable_width * 0.5)
 
         self.box2.setGeometry(10, box_y, box2_width, box_height)
-        self.label2.setGeometry(0, 0, box2_width, 40)
+        self.label2.setGeometry(0, 15, box2_width, 40)
         self.table_ego.setGeometry(10, 50, box2_width - 20, box_height - 60)
 
 
@@ -89,25 +116,22 @@ class MyWindow(QWidget):
         box_y = int(self.height() * 0.1) + 10
         box_height = int(self.height() * 0.85)
         usable_width = self.width() - 30
-
-        box2_width = int(usable_width * 0.6)
-        box3_width = int(usable_width * 0.4)
-
+        box2_width = int(usable_width * 0.5)
+        box3_width = int(usable_width * 0.5)
         x = 10 + box2_width + 10
-        self.box3.setGeometry(x, box_y, box3_width, box_height)
-        self.label3.setGeometry(0, 0, box3_width, 40)
 
-        # SensorConfigMsgStatus Werte
-        self.table_cfg = QTableWidget(7, 2, self.box3)
-        self.table_cfg.setHorizontalHeaderLabels(["Signalname", "Status"])
+        self.box3.setGeometry(x, box_y, box3_width, box_height)
+        self.label3.setGeometry(0, 15, box3_width, 40)
+
+        self.table_cfg = QTableWidget(7, 3, self.box3)
+        self.table_cfg.setHorizontalHeaderLabels(["Signalname", "Status", "Description"])
         self.table_cfg.setShowGrid(False)
         self.table_cfg.setFrameStyle(QFrame.NoFrame)
         self.table_cfg.verticalHeader().setVisible(False)
         self.table_cfg.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         header = self.table_cfg.horizontalHeader()
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QHeaderView.Stretch)
+        header.setSectionResizeMode(QHeaderView.Fixed)
 
         signal_names = [
             "LongitudinalVelocity", "LongitudinalAcceleration",
@@ -117,18 +141,77 @@ class MyWindow(QWidget):
         ]
 
         for i, name in enumerate(signal_names):
-            self.table_cfg.setItem(i, 0, QTableWidgetItem(name))
+            item = QTableWidgetItem(name)
+            item.setFont(self.gui_font)
+            self.table_cfg.setItem(i, 0, item)
 
         self.table_cfg.setGeometry(10, 50, box3_width - 20, box_height - 60)
 
-    # Echtzeitaktualsieirung der Statuswerte in der GUI
+
+    # --- Spaltenbreiten proportional anpassen ---
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        # Tabelle in Box2 (Ego) 70/30
+        ego_width = self.table_ego.viewport().width()
+        self.table_ego.setColumnWidth(0, int(ego_width * 0.7))
+        self.table_ego.setColumnWidth(1, int(ego_width * 0.3))
+
+        # Tabelle in Box3 (Config) 70/30
+        cfg_width = self.table_cfg.viewport().width()
+        self.table_cfg.setColumnWidth(0, int(cfg_width * 0.4))
+        self.table_cfg.setColumnWidth(1, int(cfg_width * 0.2))
+        self.table_cfg.setColumnWidth(2, int(cfg_width * 0.4))
+
+
+    # Live-Updates der Werte
     def update_values(self, values):
         for i, val in enumerate(values):
             item = QTableWidgetItem(val)
-            item.setTextAlignment(Qt.AlignHCenter)
+            item.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+            item.setFont(self.gui_font)
+
+            if values[i] == '00':
+                item.setForeground(QBrush(QColor("green")))
+            else:
+                item.setForeground(QBrush(QColor("red")))
+
             self.table_cfg.setItem(i, 1, item)
 
+            desc_text = self.sensorcfgmsgstatus_description(int(values[i]))
+            desc_item = QTableWidgetItem(desc_text)
+            desc_item.setFont(self.gui_font)
+            desc_item.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+            self.table_cfg.setItem(i, 2, desc_item)
 
+    def sensorcfgmsgstatus_description(self, value):
+        if value == 0:
+            return "STATE VALID"
+        elif value == 1:
+            return "STATE INVALID"
+        elif value == 2:
+            return "STATE NOT AVAILABLE"
+        elif value == 3:
+            return "STATE DECREASED"
+        elif value == 4:
+            return "STATE SUBSTITUE"
+        elif value == 5:
+            return "STATE INPLAUSIBLE"
+        elif value == 6:
+            return "STATE OF CALC"
+        elif value == 7:
+            return "STATE SENSOR"
+        elif value == 8:
+            return "STATE EXTRAPOLATED"
+        elif value == 15:
+            return "STATE INIT"
+        elif value == 255:
+            return "STATE MAX"
+        else:
+            return "UNKNOWN"
+
+
+# Main
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
--- script/SignalStateReader.py
+++ script/SignalStateReader.py
@@ -3,11 +3,11 @@ import struct
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QThread, pyqtSignal
-from SignalStateGUI import MyWindow
+from SignalStateGUI import MyWindow, apply_font_to_table
+from PyQt5.QtGui import QFont
 
 sensor_ips = {"192.168.16.15", "192.168.16.12", "192.168.16.13", "192.168.16.14"}
 
-# Multicast-Konfiguration
 MCAST_GRP = '239.22.0.3'
 UDP_PORT = 40000
 INTERFACE_IP = '192.168.16.5'
@@ -18,49 +18,43 @@ class UDPThread(QThread):
 
     def __init__(self):
         super().__init__()
-
-        # Socket konfigurieren
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(('', UDP_PORT))
-
         mreq = struct.pack("4s4s", socket.inet_aton(MCAST_GRP), socket.inet_aton(INTERFACE_IP))
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     def run(self):
         print(f"Listening on multicast {MCAST_GRP}:{UDP_PORT} ...")
-
         while True:
             data, addr = self.sock.recvfrom(4096)
-
-            # Unbekannte IPs überspringen
             if addr[0] not in sensor_ips:
                 continue
-
             raw = data.hex()
-
-            # Filter: SignalConfigMsgStatus
             if not raw.startswith("0007"):
                 continue
-
             extracted = raw[322:336]
             values = self.decode_values(extracted)
-
             self.new_values.emit(values)
 
     def decode_values(self, hexstring):
-        arr = []
-        for i in range(0, len(hexstring), 2):
-            arr.append(hexstring[i:i+2])
-        return arr
+        return [hexstring[i:i+2] for i in range(0, len(hexstring), 2)]
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    # Fenster erstellen (Font wird automatisch geladen)
     window = MyWindow()
     window.show()
 
-    # Thread starten
+    # Tabellen Font nochmal sicherstellen
+    gui_font = window.gui_font
+    apply_font_to_table(window.table_ego, gui_font)
+    apply_font_to_table(window.table_cfg, gui_font)
+
+
+    # UDP-Thread starten
     udp_thread = UDPThread()
     udp_thread.new_values.connect(window.update_values)
     udp_thread.start()
